f0a52265073ef57d8d05a9c65c576581ad57f171
Simo-2004 <153453435+Simo-2004@users.noreply.github.com>
2026-08-05 11:09:35 +0200

    fix: Updated .gitignore

--- War Advisor/debug/__init__.py
+++
@@ -1,8 +0,0 @@
-"""DEBUG TEMPORANEO (DA RIMUOVERE).
-
-Questo package contiene strumenti di debug isolati.
-La presenza di __init__.py serve solo a rendere `debug` un package Python importabile.
-Puoi eliminare l'intera cartella `debug/` in produzione.
-"""
-
-DEBUG_PACKAGE_TEMPORARY = True
